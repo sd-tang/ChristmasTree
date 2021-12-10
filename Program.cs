bbffd246b4305fc9d2401f187be92be680b67cf0
Christopher Tang <christopher.thg@gmail.com>
2021-12-11 02:02:11 +1030

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+
+namespace ChristmasTree
+{
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            MyTree(3, 9);
+        }
+
+        static void MyTree(int smallestTree, int biggestTree)
+        {
+            int currentTree = smallestTree;
+
+            while (currentTree <= biggestTree)
+            {
+                DrawTree(currentTree);
+                currentTree++;
+            }
+        }
+
+        static void DrawTree(int treeSize)
+        {
+            int whitespace = treeSize;
+
+            for (int i = 1; i <= treeSize; i++)
+            {
+                printTree(i, whitespace);
+                whitespace--;
+            }
+            printTree(1, treeSize);
+            WriteLine();
+        }
+
+        static void printTree(int iteration, int numOfSpace)
+        {
+            string currentSpace = new String(' ', numOfSpace);
+            string currentAsterisk = new String('*', iteration + (iteration - 1));
+            WriteLine(currentSpace + currentAsterisk);
+        }
+    }
+}
